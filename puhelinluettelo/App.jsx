d223790283d0fa034428987dbdee35c4fe17e9c4
Nelli Sviili <nelli.sviili@gmail.com>
2023-11-16 12:58:17 +0100

    teht 2.9 puhelinluettelo step4 valmis

--- puhelinluettelo/App.jsx
+++ puhelinluettelo/App.jsx
@@ -1,14 +1,20 @@
-// teht 2.8 puhelinluettelo step3
-// lisätään toiseen inputtiin numeroiden tallennus
-// lisätty myös uusi tapahtumakäsittelijä ja
-// addNumberiin kaikki tarpeellinen puhelinnumeron tallentamiseen
+// teht 2.9 puhelinluettelo step4
+// lisätään lomakkeelle hakukenttä, jolla rajataan näytettäviä nimiä
 
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([ { name: 'Arto Hellas' } ]) 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newCondition, setNewCondition] = useState('')
+  const [filteredPersons, setFilteredPersons] = useState(persons)
 
   const addNumber = (event) => {
     event.preventDefault()
@@ -28,7 +34,7 @@ const App = () => {
       const newObject = {
         name: newName,
         number: newNumber,
-      };
+      }
     
       setPersons(persons.concat(newObject))
       setNewName('')
@@ -46,11 +52,30 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handdleSearch = (event) => {
+    const condition = event.target.value
+    setNewCondition(condition)
+  
+    const filtered = persons.filter((person) =>
+      person.name.toLowerCase().includes(condition.toLowerCase())
+    )
+    
+    setFilteredPersons(filtered)
+  }
+  
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addNumber}>
         <div>
+          filter shown with <input
+            value={newCondition}
+            onChange={handdleSearch}
+          />
+        </div>
+        <div>
+          <h2>Add a new</h2>
           name: <input
             value={newName}
             onChange={handdleNameChange} 
@@ -63,8 +88,8 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
-    </div>
+        {filteredPersons.map((person) => (<p key={person.name}>{person.name} {person.number}</p> ))}    
+  </div>
   )
 }
 
